baadba48e971606fd8350f6270006833eb72cf66
Samuel Fern <fersa080@student.otago.ac.nz>
2021-09-30 14:49:55 +1300

    added read Data

--- Etude09_PulsesCounting/PulseCounter.java
+++ Etude09_PulsesCounting/PulseCounter.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 //https://www.gaussianwaves.com/2010/11/moving-average-filter-ma-filter-2/
 
 public class PulseCounter{
 
-    ArrayList<Integer> initialData = new ArrayList<>();
+    private static ArrayList<Integer> initialData = new ArrayList<>();
 
     public static void main(String[] args){
         
@@ -30,6 +31,19 @@ public class PulseCounter{
     }
 
     private static void readData(){
-
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()){
+            String line = sc.nextLine();
+            try {
+                initialData.add(Integer.parseInt(line));
+            } catch (NumberFormatException e){
+                System.out.println("Bad Input: Data file should only include numbers");
+                System.exit(0);
+            }
+        }
+        sc.close();
     }
+
+    
+
 }
