26d184b12ac9d7a4b4fce6e3afb153bcd73498e2
Blake MacDade <macbl788@student.otago.ac.nz>
2021-09-30 15:40:03 +1300

    started vector pass filtering

--- Etude09_PulsesCounting/PulseCounter.java
+++ Etude09_PulsesCounting/PulseCounter.java
@@ -1,26 +1,62 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Vector;
 
 //https://www.gaussianwaves.com/2010/11/moving-average-filter-ma-filter-2/
 
 public class PulseCounter{
 
     private static ArrayList<Integer> initialData = new ArrayList<>();
+    private static boolean debug = false;
 
     public static void main(String[] args){
+        if(args.length > 0){
+            if(args[0].charAt(0) == 'd' || args[0].charAt(0) == 'D'){
+                debug = true;
+            }
+        }
+        readData();
+        System.out.println("Number of Peaks in initialData: "+countNumberOfPeaks(initialData));
+        ArrayList<Integer> filteredData = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            if(i == 0) filteredData = movingAverageFilterPass(initialData);
+            else filteredData = movingAverageFilterPass(filteredData);
+        }
         
+        System.out.println("Number of peaks after filtering: "+countNumberOfPeaks(filteredData));
+        filteredData.forEach((data)->System.out.println(data));
+        //initialData.forEach((data)->System.out.println(data));
+
 
     }
 
-    public static ArrayList<Integer> movingAverageFilterPass(ArrayList<Integer> dataArray){
+    public static ArrayList<Integer> vectorFilteringPass(ArrayList<Integer> dataArray){
+        if(debug) System.out.println("in vector filter");
         ArrayList<Integer> outArray = new ArrayList<>();
         for(int i = 0; i < dataArray.size();i++){
+            if(debug) System.out.println("i: "+ i+" size of array: "+dataArray.size());
             if(i == 0){
+                Vector<Integer>  dataVec = new Vector2(dataArray.get(i), dataArray.get(i+1));
+                Vector<Float> smoothingVector = new Vector<>(0.8,0.1);
+            }
+        }
+    }
+
+
+    public static ArrayList<Integer> movingAverageFilterPass(ArrayList<Integer> dataArray){
+        if(debug) System.out.println("In filter!");
+        ArrayList<Integer> outArray = new ArrayList<>();
+        for(int i = 0; i < dataArray.size();i++){
+            if(debug) System.out.println("i: "+ i+" size of array: "+dataArray.size());
+            if(i == 0){
+                if(debug) System.out.println("i = 0");
                 outArray.add(i,(dataArray.get(i) + dataArray.get(i+1))/2);
             }else if(i == dataArray.size()-1){
+                if(debug) System.out.println("at last index");
                 outArray.add(i,(dataArray.get(i) + dataArray.get(i-1))/2);
             }else{
-                outArray.add(i,sum(outArray.get(i-1),outArray.get(i),outArray.get(i+1)));
+                if(debug) System.out.println("anything else");
+                outArray.add(i,sum(dataArray.get(i-1),dataArray.get(i),dataArray.get(i+1)));
             }
         }
         return outArray;
@@ -28,10 +64,25 @@ public class PulseCounter{
 
     private static int countNumberOfPeaks(ArrayList<Integer> data){
         int average = calculateAverage(data);
+        System.out.println("Average: "+average);
         int offset = 0;
         int peaks = 0;
         for(int i = 0; i < data.size();i++){
-            if(data.get(i) >= average+offset) peaks++;
+            if(i == 0){
+                if(data.get(i) > data.get(i+1)){
+                    peaks++;
+                }
+            }else if (i == data.size()-1){
+                if(data.get(i) > data.get(i-1)){
+                    peaks++;
+                }
+            }
+            else{
+                if(data.get(i) > data.get(i-1) && data.get(i) > data.get(i+1)){
+                    peaks++;
+                }
+            }
+            //if(data.get(i) >= average+offset) peaks++;
         }
         return peaks;
 
