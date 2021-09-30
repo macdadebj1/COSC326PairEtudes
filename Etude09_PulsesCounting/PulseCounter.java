e9671ccec4fea85d8a20803fec9074eff04faa6b
Blake MacDade <macbl788@student.otago.ac.nz>
2021-09-30 14:18:01 +1300

    start

--- Etude09_PulsesCounting/PulseCounter.java
+++ Etude09_PulsesCounting/PulseCounter.java
@@ -1,12 +1,19 @@
+import java.util.ArrayList;
 
 public class PulseCounter{
 
-    public static void main(String[] args){
+    ArrayList<Integer> initialData = new ArrayList<>();
 
+    public static void main(String[] args){
+        
 
     }
 
     public static void vectorFilter(){
+
+    }
+
+    private static void readData(){
 
     }
 }
