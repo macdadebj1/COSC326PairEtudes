467c10dabf79519d7e8ce1d96ceddd0cb3e5c7a0
Blake <macdadeblake@gmail.com>
2021-10-06 21:59:37 +1300

    made changes to matlab code

--- Etude09_PulsesCounting/plotSignal.m
+++ Etude09_PulsesCounting/plotSignal.m
@@ -4,22 +4,22 @@ formatSpec = '%d';
 size = [1 Inf];
 data = fscanf(file,formatSpec,size);
 
-sampleFrequency = 10;
-order = 3;
-lowCutFrequency = 1.2;
-highCutFrequency = 2.25;
+sampleFrequency = 200;
+order = 10;
+lowCutFrequency = 1.5;
+highCutFrequency = 7.5;
 %flag = 'scale';
 
 window = blackman(order+1);
 
-bandPass = fir1(order, [lowCutFrequency highCutFrequency]/(sampleFrequency/2), 'bandpass',window, 'scale');
+bandPass = fir1(order, [lowCutFrequency highCutFrequency]/(sampleFrequency/2), 'bandpass',window, 'scale'); 
 discreteTimeFilter = dfilt.dffir(bandPass);
 filteredData = filter(discreteTimeFilter,data);
 %bandP = bandpass(data, [500,800], 1000);
 
 
 hold on;
-plot(data);
+%plot(data);
 plot(filteredData);
 %plot(bandP);
 hold off;
