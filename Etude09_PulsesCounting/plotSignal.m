bb355492c72c3be8bdf4a8c140a9ceba750b17d2
Blake <macdadeblake@gmail.com>
2021-10-06 15:09:17 +1300

    updated matlab code, can now perform bandpass filtering

--- Etude09_PulsesCounting/plotSignal.m
+++ Etude09_PulsesCounting/plotSignal.m
@@ -1,10 +1,35 @@
 function z = plotSignal(filename)
-
 file = fopen(filename,'r');
 formatSpec = '%d';
 size = [1 Inf];
 data = fscanf(file,formatSpec,size);
 
-plot(data);
+sampleFrequency = 200;
+order = 10;
+lowCutFrequency = 1.5;
+highCutFrequency = 7.5;
+%flag = 'scale';
 
+window = blackman(order+1);
+
+bandPass = fir1(order, [lowCutFrequency highCutFrequency]/(sampleFrequency/2), 'bandpass',window, 'scale');
+filterWrapper = dfilt.dffir(bandPass);
+res = filter(filterWrapper,data);
+%bandP = bandpass(data, [500,800], 1000);
+
+
+hold on;
+plot(data);
+plot(res);
+%plot(bandP);
+hold off;
+count = 0;
+for n=2: length(res)-1
+   if res(n) > res(n-1)
+       if res(n) > res(n+1)
+           count = count +1;
+       end    
+   end    
+end
+z = count;
 end
