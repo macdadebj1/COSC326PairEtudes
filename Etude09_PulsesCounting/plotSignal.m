d558f4fda78556f5b3b23bd85e0c07189d2e14ce
Blake <macdadeblake@gmail.com>
2021-10-06 15:14:00 +1300

    renamed variables

--- Etude09_PulsesCounting/plotSignal.m
+++ Etude09_PulsesCounting/plotSignal.m
@@ -12,21 +12,21 @@ highCutFrequency = 7.5;
 
 window = blackman(order+1);
 
-bandPass = fir1(order, [lowCutFrequency highCutFrequency]/(sampleFrequency/2), 'bandpass',window, 'scale');
-filterWrapper = dfilt.dffir(bandPass);
-res = filter(filterWrapper,data);
+bandPass = fir1(order, [lowCutFrequency highCutFrequency]/(sampleFrequency/2), 'bandpass',window, 'scale'); $
+discreteTimeFilter = dfilt.dffir(bandPass);
+filteredData = filter(discreteTimeFilter,data);
 %bandP = bandpass(data, [500,800], 1000);
 
 
 hold on;
 plot(data);
-plot(res);
+plot(filteredData);
 %plot(bandP);
 hold off;
 count = 0;
-for n=2: length(res)-1
-   if res(n) > res(n-1)
-       if res(n) > res(n+1)
+for n=2: length(filteredData)-1
+   if filteredData(n) > filteredData(n-1)
+       if filteredData(n) > filteredData(n+1)
            count = count +1;
        end    
    end    
